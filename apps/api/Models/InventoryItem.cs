bcf124b6184279c3fd0935b30760f3eeaa9f6259
Steve Hatherley <steve.hatherley@gmail.com>
2023-02-28 09:03:36 -0600

    InventoryItem model

---
+++ apps/api/Models/InventoryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Init.Api.Models
+{
+    public partial class InventoryItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Description { get; set; } = null!;
+        public short Quantity { get; set; }
+    }
+}
